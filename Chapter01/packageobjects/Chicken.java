f69829c6ce6b8f8dfb3e8052600cbc6aa5893698
Fernanda Reis <feevars@gmail.com>
2024-03-24 21:27:22 -0300

    creating jar files and class information from chapter 01 until pg 28

---
+++ Chapter01/packageobjects/Chicken.java
@@ -0,0 +1,8 @@
+public class Chicken {
+    int numOfWings = 2;
+    String name;
+
+    public Chicken() {
+        name = "Marilu";
+    }
+}
---
+++ Chapter01/packageobjects/Park.java
@@ -0,0 +1,6 @@
+public class Park {
+    public Park() {
+        System.out.println("I'm a constructor for class Park.");
+    }
+
+}
