096c2203bac9ddbaedff221d66ed6b032a5639e1
Fernanda Reis <feevars@gmail.com>
2024-03-27 21:37:41 -0300

    Added number and underscore example

---
+++ Chapter01/packageliterals/WritingLiterals.java
@@ -0,0 +1,8 @@
+public class WritingLiterals {
+    
+double notAtStart = _1200.00; // não compila
+double notAtEnd = 1000.00_); // não compila
+double notByDecimal = 1000_.00 // não compila
+double notByDecimal2 = 1000._0; // não compila
+double compiles = 1_000_000.00;
+}
